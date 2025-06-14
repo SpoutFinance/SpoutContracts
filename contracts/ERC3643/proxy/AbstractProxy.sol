1d6112a679f827350782bee2f30ae34251187774
Paul <mierlo.paul@hotmail.nl>
2025-06-14 16:06:39 +0200

    added full suite of t-rex

--- contracts/ERC3643/proxy/AbstractProxy.sol
+++ contracts/ERC3643/proxy/AbstractProxy.sol
@@ -36,7 +36,6 @@
 //                                        +@@@@%-
 //                                        :#%%=
 //
-
 /**
  *     NOTICE
  *
@@ -63,39 +62,51 @@
 
 pragma solidity 0.8.17;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "./interface/IProxy.sol";
+import "./authority/ITREXImplementationAuthority.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
-import "./Roles.sol";
+abstract contract AbstractProxy is IProxy, Initializable {
 
-contract AgentRole is Ownable {
-    using Roles for Roles.Role;
-
-    Roles.Role private _agents;
-
-    event AgentAdded(address indexed _agent);
-    event AgentRemoved(address indexed _agent);
-
-    modifier onlyAgent() {
+    /**
+     *  @dev See {IProxy-setImplementationAuthority}.
+     */
+    function setImplementationAuthority(address _newImplementationAuthority) external override {
+        require(msg.sender == getImplementationAuthority(), "only current implementationAuthority can call");
+        require(_newImplementationAuthority != address(0), "invalid argument - zero address");
         require(
-            isAgent(msg.sender),
-            "AgentRole: caller does not have the Agent role"
-        );
-        _;
+            (ITREXImplementationAuthority(_newImplementationAuthority)).getTokenImplementation() != address(0)
+            && (ITREXImplementationAuthority(_newImplementationAuthority)).getCTRImplementation() != address(0)
+            && (ITREXImplementationAuthority(_newImplementationAuthority)).getIRImplementation() != address(0)
+            && (ITREXImplementationAuthority(_newImplementationAuthority)).getIRSImplementation() != address(0)
+            && (ITREXImplementationAuthority(_newImplementationAuthority)).getMCImplementation() != address(0)
+            && (ITREXImplementationAuthority(_newImplementationAuthority)).getTIRImplementation() != address(0)
+        , "invalid Implementation Authority");
+        _storeImplementationAuthority(_newImplementationAuthority);
+        emit ImplementationAuthoritySet(_newImplementationAuthority);
     }
 
-    function addAgent(address _agent) public onlyOwner {
-        require(_agent != address(0), "invalid argument - zero address");
-        _agents.add(_agent);
-        emit AgentAdded(_agent);
+    /**
+     *  @dev See {IProxy-getImplementationAuthority}.
+     */
+    function getImplementationAuthority() public override view returns(address) {
+        address implemAuth;
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            implemAuth := sload(0x821f3e4d3d679f19eacc940c87acf846ea6eae24a63058ea750304437a62aafc)
+        }
+        return implemAuth;
     }
 
-    function removeAgent(address _agent) public onlyOwner {
-        require(_agent != address(0), "invalid argument - zero address");
-        _agents.remove(_agent);
-        emit AgentRemoved(_agent);
+    /**
+     *  @dev store the implementationAuthority contract address using the ERC-3643 implementation slot in storage
+     *  the slot storage is the result of `keccak256("ERC-3643.proxy.beacon")`
+     */
+    function _storeImplementationAuthority(address implementationAuthority) internal {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            sstore(0x821f3e4d3d679f19eacc940c87acf846ea6eae24a63058ea750304437a62aafc, implementationAuthority)
+        }
     }
 
-    function isAgent(address _agent) public view returns (bool) {
-        return _agents.has(_agent);
-    }
 }
