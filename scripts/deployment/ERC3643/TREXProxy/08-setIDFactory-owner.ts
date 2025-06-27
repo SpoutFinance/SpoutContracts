bb426fa1350f4fb1214d6b68668c0d75aaeea033
Paul <mierlo.paul@hotmail.nl>
2025-06-27 13:14:35 +0200

    organised deployment structure

--- scripts/deployment/ERC3643/TREXProxy/08-setIDFactory-owner.ts
+++ scripts/deployment/ERC3643/TREXProxy/08-setIDFactory-owner.ts
@@ -2,9 +2,8 @@ import { ethers } from "hardhat"
 
 async function main() {
   const ID_FACTORY_ADDRESS = "0xb04eAce0e3D886Bc514e84Ed42a7C43FC2183536" // your IdFactory address
-  const TREX_FACTORY_ADDRESS = "0x50c88ac21f82AC78A4A72BD30fbbfc70239cA899" // your TREXFactory address
+  const TREX_FACTORY_ADDRESS = "0x2Eac68d74c552E86b6EF6888b3E18817fAde1785" // your TREXFactory address
 
-  const [deployer] = await ethers.getSigners()
   console.log("Registering TREXFactory as token factory in IdFactory...")
   const IdFactory = await ethers.getContractFactory("IdFactory")
   const idFactory = IdFactory.attach(ID_FACTORY_ADDRESS)
