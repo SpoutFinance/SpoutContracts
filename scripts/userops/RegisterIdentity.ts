32a291bab1862d61e1c0328ff2917eb2e973eb76
Paul <mierlo.paul@hotmail.nl>
2025-06-27 12:49:11 +0200

    organized deployment scripts

--- scripts/userops/RegisterIdentity.ts
+++ scripts/userops/RegisterIdentity.ts
@@ -4,9 +4,9 @@ async function main() {
   // --- Paste your deployed IdentityRegistryProxy address here ---
   const identityRegistryAddress = "0x296D988cd8193D5c67a71A68E9Bdf533f53f943E" // <-- Replace if needed
   // --- Paste the user's wallet address here ---
-  const userWalletAddress = "0x369B11fb8C65d02b3BdD68b922e8f0D6FDB58717" // <-- Replace with the user's wallet address
+  const userWalletAddress = "0x23EBeA62B3dB762475Db41Dc41eDa7e2021e1C55" // <-- Replace with the user's wallet address
   // --- Paste the ONCHAINID address here ---
-  const onchainIdAddress = "0x2eC77FDcb56370A3C0aDa518DDe86D820d76743B" // <-- Replace with the ONCHAINID address
+  const onchainIdAddress = "0xfBbB54Ea804cC2570EeAba2fea09d0c66582498F" // <-- Replace with the ONCHAINID address
   // --- Set the country code (uint16) ---
   const country = 1 // <-- Replace with the user's country code
 
